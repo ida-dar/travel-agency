e17cff112b0a825970c0c48511480e2c770932a2
Ida Darmochwał <ida.mdart@gmail.com>
2021-07-27 21:58:28 +0200

    Send trip's order data to API

--- src/components/features/OrderForm/OrderForm.js
+++ src/components/features/OrderForm/OrderForm.js
@@ -1,15 +1,56 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-//import styles from './OrderForm.scss';
-import { Row, Col } from 'react-flexbox-grid';
-import OrderSummary from '../OrderSummary/OrderSummary';
+import settings from '../../../data/settings';
 import pricing from '../../../data/pricing.json';
-import OrderOption from '../OrderOption/OrderOption';
-//import { setOrderOption } from '../../../redux/orderRedux';
+import { Row, Col } from 'react-flexbox-grid';
 
-const OrderForm = ({tripCost, options, setOrderOption}) => {
+import { formatPrice } from '../../../utils/formatPrice';
+import { calculateTotal } from '../../../utils/calculateTotal';
+
+import OrderSummary from '../OrderSummary/OrderSummary';
+import OrderOption from '../OrderOption/OrderOption';
+import Button from '../../common/Button/Button';
+
+const sendOrder = (options, tripCost, tripId, tripName, countryCode) => {
+  const totalCost = formatPrice(calculateTotal(tripCost, options));
+
+  const payload = {
+    ...options,
+    totalCost,
+    tripId,
+    tripName,
+    countryCode,
+  };
+
+  //console.log(payload);
+
+  if(options.name === '' && options.contact === ''){
+    alert('Please enter your contact data');
+    return;
+  }
+
+  const url = settings.db.url + '/' + settings.db.endpoint.orders;
+
+  const fetchOptions = {
+    cache: 'no-cache',
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(payload),
+  };
+
+  fetch(url, fetchOptions)
+    .then(function(response){
+      return response.json();
+    }).then(function(parsedResponse){
+      console.log('parsedResponse', parsedResponse);
+    });
+};
+
+const OrderForm = ({tripCost, options, setOrderOption, countryCode, tripName, tripId}) => {
   //console.log(`pricing:`, pricing, `options:`, options, tripCost);
-  
+
   return (
     <Row>
       {pricing.map(option => (
@@ -25,6 +66,7 @@ const OrderForm = ({tripCost, options, setOrderOption}) => {
       <Col xs={12}>
         <OrderSummary tripCost={tripCost} options={options} />
       </Col>
+      <Button onClick={() => sendOrder(options, tripCost, tripId, tripName, countryCode)}>Order now!</Button>
     </Row>
   );
 };
@@ -33,6 +75,9 @@ OrderForm.propTypes = {
   tripCost: PropTypes.string,
   options: PropTypes.object,
   setOrderOption: PropTypes.func,
+  countryCode: PropTypes.string,
+  tripName: PropTypes.string,
+  tripId: PropTypes.string,
 };
 
 export default OrderForm;
